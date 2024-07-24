4468f00f8327f64a69d2426ff8239a2d53918540
blackbrokkoli <kastomat@gmail.com>
2024-07-24 22:04:20 +0300

    add dialogue manager but broken

--- doc/DOCUMENTATION.md
+++ doc/DOCUMENTATION.md
@@ -3,3 +3,6 @@ aliases:
 created-at: 20.07.2024
 q-type: 
 ---
+
+
+- using [this dialog tut](https://www.youtube.com/watch?v=Q8iuaHwzxWY)
--- scenes/tutor.gd
+++ scenes/tutor.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
+
+var dia = load("res://dialog/main.dialogue")
 func _ready():
 	animated_sprite_2d.play("idle")
+	
+	var dialogue_line = await DialogueManager.show_dialogue_balloon(dia, "demo")
