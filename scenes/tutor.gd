83a84142817581255daa390ae50e750ee90f1546
blackbrokkoli <kastomat@gmail.com>
2024-07-28 16:11:12 +0300

    hotfix tutor trying to walk through walls

--- scenes/tutor.gd
+++ scenes/tutor.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
-const speed = 20
+const speed = 50
 
 @export var node_destination: Node2D
 @onready var nav_agent = $NavigationAgent2D
